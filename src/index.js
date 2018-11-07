bccc1110f78eb2e90b589ef113506624e5f01ad2
Diego Javier <39574881+diegojp747@users.noreply.github.com>
2018-11-07 21:52:24 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+import "tachyons";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
